e990f57266a38768b30880a45d356b049ecefb0f
kamika9ze <kamika9ze@gmail.com>
2020-02-04 20:27:55 +0600

    Initial commit

---
+++ src/components/List/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import classNames from 'classnames';
+import removeSvg from '../../assets/img/remove.svg';
+import Badge from '../Badge/';
+import './List.scss';
+import '../Badge/Badge.scss';
+
+
+const List = ({items, isRemovable, onClick }) => {
+    return (
+        <ul onClick={onClick} className="list">
+            {items.map((item, index) => (
+                    <li key={index} className={classNames(item.className, {'active' : item.active})}>
+                        <i>
+                            {item.icon ? (
+                                item.icon
+                            ) : (
+                                <Badge color={item.color} />
+                            )}
+                        </i>
+                        <span>{item.name}</span>
+                        {isRemovable && (
+                            <img
+                            className="list__remove-icon"
+                            src={removeSvg}
+                            alt="Remove icon"
+                            // onClick={() => removeList(item)}
+                            />
+                        )}
+                    </li>
+                ) )
+            }
+        </ul>
+    )
+}
+
+export default List;
